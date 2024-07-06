aeff5f906767e1a8ad0535c8d0fe5092f5c25d52
kamlesh <kamleshchandel17@gmail.com>
2024-07-06 12:20:13 +0530

    last commit

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import './Header.css'
 
 export default function Header() {
+  const navigate = useNavigate()
+  const onClickLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/register");
+  }
   return (
     <div className='header'>
       <h1 className='logo'>CROSS PE</h1>
       <div className='navcontainer'>
         <Link to="/" ><p className='classname'>Classes</p></Link>
         <Link to="/bookings"><p className='classname'>Bookings</p></Link>
+        <button className='logout' onClick={onClickLogout}>Log Out</button>
       </div>
     </div>
   )
